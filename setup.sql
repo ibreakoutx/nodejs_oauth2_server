a273080f68efe9ea47eab974a0800d702ba05468
ibreakoutx <ibreakoutx@gmail.com>
2018-09-01 19:51:00 +0000

    fixed database name in setup.sql

--- setup.sql
+++ setup.sql
@@ -1,9 +1,7 @@
-#mysql -u root -p  
-#password : mysql
+create database oAuth2Test;
 
-create database oauth2Test;
 show databases;
-use oauth2Test;
+use oAuth2Test;
 
 create table users ( id int(16) not null auto_increment, username varchar(255), user_password varchar(255) ,primary key (id) );
 create table access_tokens ( access_token varchar(255), user_id int(16) );
